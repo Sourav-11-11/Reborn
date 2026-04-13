487a979b96d95725ea9fc55ab0b13ac99ed996b0
Sourav <sourav@example.com>
2026-04-14 03:06:02 +0530

    Fixed Mongo connection

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Product = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://localhost:27017/reborn";
+const MONGO_URL = process.env.ATLAS_URL || process.env.MONGO_URL || "mongodb://localhost:27017/reborn";
 
 main()
     .then(() => {
